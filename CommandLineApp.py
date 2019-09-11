bc1f4096a0f2bf9e3ab4231f798de3041a0051ef
Rishabh Patel <42808964+rishabhpatel11@users.noreply.github.com>
2019-09-10 21:39:51 -0400

    Add files via upload

--- CommandLineApp.py
+++ CommandLineApp.py
@@ -3,46 +3,56 @@ import SplitTipCalculator
 import BMICalculator
 import DistanceCalculator
 
-print('Enter a number to select a function from the list:')
-print('1. Body Mass Index Calculator')
-print('2. Retirement Age Calculator')
-print('3. Shortest Distance Calculator')
-print('4. Split the Tip')
+def function():
+    print('Enter a number to select a function from the list:')
+    print('1. Body Mass Index Calculator')
+    print('2. Retirement Age Calculator')
+    print('3. Shortest Distance Calculator')
+    print('4. Split the Tip')
+    choice=(int)(input("-->"))
+    if(choice == 1):
+        feet=input("Enter the feet part of your total height as a whole number: \n")
+        inches=input("Enter the inches part of your total height as a whole number: \n")
+        weight=input("Enter your weight in pounds as a number: \n")
+        output=BMICalculator.bmi(feet,inches,weight)
+        print("Your Body Mass Index is: " + str(output[0]))
+        print("Your Body Mass Index falls under the category: " + output[1])
+        
+    if(choice == 2):
+        age=input("Enter your age as a whole number: \n")
+        salary=input("Enter your annual salary as a number: \n")
+        saved=input("Enter the percent of your salary you plan on saving yearly as a number: \n")
+        goal=input("Enter your desired retirement savings goal as a number: \n")
+        output=RetirementCalculator.retirement(age,salary,saved,goal)
+        if(output[1] == -1):
+            print(output[0])
+        else:
+            print(output[0] + str(output[1]))
 
-choice=(int)(input("-->"))
+    if(choice == 3):
+        print("Refer to the 2 points as: (x1,y1) and (x2,y2)")
+        x1=input("Enter x1: \n")
+        y1=input("Enter y1: \n")
+        x2=input("Enter x2: \n")
+        y2=input("Enter y2: \n")
+        print("The shortest distance between the points is: " + str(DistanceCalculator.distance(x1,y1,x2,y2)))
 
-if(choice == 1):
-    feet=input("Enter the feet part of your total height as a whole number: \n")
-    inches=input("Enter the inches part of your total height as a whole number: \n")
-    weight=input("Enter your weight in pounds as a number: \n")
-    output=BMICalculator.bmi(feet,inches,weight)
-    print("Your Body Mass Index is: " + str(output[0]))
-    print("Your Body Mass Index falls under the category: " + output[1])
-    
-if(choice == 2):
-    age=input("Enter your age as a whole number: \n")
-    salary=input("Enter your annual salary as a number: \n")
-    saved=input("Enter the percent of your salary you plan on saving yearly as a number: \n")
-    goal=input("Enter your desired retirement savings goal as a number: \n")
-    output=RetirementCalculator.retirement(age,salary,saved,goal)
-    if(output[1] == -1):
-        print(output[0])
-    else:
-        print(output[0] + str(output[1]))
+    if(choice == 4):
+        print("Calculator adds a 15% tip rounded to the nearest cent")
+        total=input("Enter the total of the bill as a number without any $ signs: \n")
+        guests=input("Enter the number of guests you want to split the bill for: \n")
+        output=SplitTipCalculator.splitTip(total,guests)
+        print("Total with tip: " + str(output[len(output)-1]))
+        for i in range(len(output)-1):
+            print("guest"+str(i)+"-"+str(output[i]))
+    print("\n\nEnter 1 to exit now or 0 to return to the menu")
+    global exit
+    exit=(int)(input("-->"))
+    if(exit != 1 and exit !=0):
+        print("Invalid choice, exiting application...")
+    if(exit ==0 ):
+        print("\n\n\n")
 
-if(choice == 3):
-    print("Refer to the 2 points as: (x1,y1) and (x2,y2)")
-    x1=input("Enter x1: \n")
-    y1=input("Enter y1: \n")
-    x2=input("Enter x2: \n")
-    y2=input("Enter y2: \n")
-    print("The shortest distance between the points is: " + str(DistanceCalculator.distance(x1,y1,x2,y2)))
-
-if(choice == 4):
-    print("Calculator adds a 15% tip rounded to the nearest cent")
-    total=input("Enter the total of the bill as a number without any $ signs: \n")
-    guests=input("Enter the number of guests you want to split the bill for: \n")
-    output=SplitTipCalculator.splitTip(total,guests)
-    print("Total with tip: " + str(output[len(output)-1]))
-    for i in range(len(output)-1):
-        print("guest"+str(i)+"-"+str(output[i]))
+exit=0
+while(exit==0):
+    function()
--- readme.md
+++ readme.md
@@ -2,18 +2,28 @@
 Project Report
 Rishabh Patel
 
+# Naming Conventions
+Each function has a testing file associated with it. Pytest requires 'test_' before or after
+the file name and individual unit test name. I decided to put the keyword in the front so the test files 
+are named as such: 'test_bmi'. Individual unit tests also follow the format of 'test_'+'NameOfTest'.
+
+Pytest doesn't allow a descriptor for tests so I tried to keep the test names as descriptive as possible
+to fill this void. The two types of tests I used are ones that assert whether something is true or
+ones that check whether a specific error/exception is raised. The names indicicate whether it was testing for
+success or failure.
+
 # Environment & Tools used
 OS- Windows 10
-Language- Python
-Verison- Python 3.6.1
-Testing Tools- pytest and pytest-cov
+Language- Python 3.6.1
+Testing Tools- pytest 
+Coverage Tool- pytest-cov
 
 ## Installation
 *   [Python 3.6.1](https://www.python.org/downloads/release/python-361/)- This version was used to write 
 the code so it is recommended, but any Python 3 should work. 
 Make sure to check the "add python to path" option when installing.
 
-The code has been tested on Python 2.7.16 and all the tests will work. But, when using the
+If you already have a version of Python 2 installed, the code should still work but for the
 CLI, make sure to enter all inputs surrounded by "" since the input function has been changed
 from Python 2 to Python 3.
 
@@ -32,6 +42,11 @@ From a command prompt, go to the location of the test files you downloaded from 
 ```bash
 Example- cd C:\Users\Rishi\Downloads\PPA#1Python
 ```
+
+To run all test files and see the results of each individual unit test, use the
+following command:
+
+    pytest -rA
 
 Below is an example of how to run a specific test from the command prompt. 
 Replace the filename with the name of the file you want to test. 
@@ -55,19 +70,25 @@ python CommandLineApp.py
 ```
 
 # Opinions on TDD
-I have done unit testing in java but it was very brief and a long time ago. This is the first time I am using TDD. 
+I haved used unit testing in java but it was a long time ago and very brief. For my first time unit testing in python, I found it
+to be fairly straightforward and simple. Each test was logically understandable and only required a few lines to set up.
+This is the first time I am using TDD. I would say TDD was helpful to make sure my functions fulfilled the requirements, but
+it was also a little annoying to deal with, when sometimes I write a test for what ends up being not even a full line of code.
 I definitely think unit testing is helpful in general, whether it be with TDD or not. For most software, unit testing is 
 probably necessary since there are many parts that need to be tested individually. 
 
-As for TDD, the main advantages I noticed for me were increased organization in code and minimal code. Writing the code, 
+For TDD, the main advantages I noticed for me were increased organization in code and minimization of code. Writing the code, 
 I went in order of the tests, so everything just naturally grouped up like all the data type tests are grouped at the top. 
 Also, I only wrote code that was necessary to pass the tests so I didn't end up with any unused variables or useless lines. 
 
 The major drawback of TDD was slowness. For most of my functions, the test files had more lines of code than the function it was testing.
 To get a high code coverage, you need to write tests for simple code that you already know works and it just seems more like a formality to test it.
+The other big disadvantage I ran into was having tests cause other tests to fail. One test requires a certain change, like changing my output type 
+from a number to a string. Then, I would have to go through the other tests and change them as well.
+
 In a real project, TDD's usefulness would depend on the project itself. If it was part of a large company where all the functions and 
 features are already specified and formalized, then I think TDD would be beneficial. If it was just a solo project or a small team, 
-I think it might be better for the flow of the project to jump right into the code.
+I think it might be better for the flow of the project to jump right into the code. 
 
 
 "# PPA1_Rishabh_Patel" 
