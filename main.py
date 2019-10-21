9c0841728be7cfc01862d9be3e4eafab47439068
Rishabh Patel <rishi333@ufl.edu>
2019-10-21 02:45:21 -0400

    final build

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import subprocess
-path = r"C:\Users\Rishi\Documents\GitHub\PPA1_Rishabh_Patel"
+import os
+path = os.getcwd()
 processes = ['WebApp.py', 'CommandLineApp.py']
 p1=subprocess.Popen(r'python %s\%s' % (path, processes[0]), shell=True)
 p2=subprocess.Popen(r'python %s\%s' % (path, processes[1]), shell=True)
